15eb28ec173d7b952fac10f45ab81c0f50cf454a
sangumee <super2451894@gmail.com>
2018-07-24 22:02:10 +0900

    Change Token File
    Change Token File

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
-const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/36cb6033-b459-4d75-b2cf-dbc9c2346392/token";
+const instanceLocator = "v1:us1:36cb6033-b459-4d75-b2cf-dbc9c2346392";
 
 export { tokenUrl, instanceLocator }
