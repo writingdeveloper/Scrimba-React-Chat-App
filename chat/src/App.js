1da2d30a85ce5173aa2616e2fd19da15e32c6ab3
sangumee <super2451894@gmail.com>
2018-07-25 17:26:44 +0900

    Reinstall NPM
    Reinstall NPM

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit-server';
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import {tokenUrl, instanceLocator} from './config'
+
 class App extends React.Component {
+
+    componentDidMount(){
+        const chatManager= new Chatkit.ChatManager({
+            instanceLocator,
+            userId:'sangumee',
+            tokenProvider:new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+        })
+        chatManager.connect()
+        .then(currentUser =>{
+            currentUser.subscribeToRoom({
+                roomId:12119915,
+                hooks:{
+                    onNewMessage: message =>{
+                        console.log('message.text :', message.text);
+                    }
+                }
+            })
+        })
+    }
+
     render() {
         return (
             <div className="app">
