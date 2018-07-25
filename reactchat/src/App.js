55a90b8d538b34c2ce051cef013d69c223c59b40
sangumee <super2451894@gmail.com>
2018-07-25 17:40:14 +0900

    Success!!
    Success!!

--- reactchat/src/App.js
+++ reactchat/src/App.js
@@ -1,35 +1,36 @@
 import React from 'react'
-import Chatkit from '@pusher/chatkit-server';
+import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
-import {tokenUrl, instanceLocator} from './config'
+import { tokenUrl, instanceLocator } from './config'
 
 class App extends React.Component {
 
-    componentDidMount(){
-        const chatManager= new Chatkit.ChatManager({
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
             instanceLocator,
-            userId:'sangumee',
-            tokenProvider:new Chatkit.TokenProvider({
+            userId: 'sangumee', /** swap out */
+            tokenProvider: new Chatkit.TokenProvider({
                 url: tokenUrl
             })
         })
+        
         chatManager.connect()
-        .then(currentUser =>{
+        .then(currentUser => {
             currentUser.subscribeToRoom({
-                roomId:12119915,
-                hooks:{
-                    onNewMessage: message =>{
-                        console.log('message.text :', message.text);
+                roomId: 12119915, /** swap out */
+                hooks: {
+                    onNewMessage: message => {
+                        console.log('message.text: ', message.text);
                     }
                 }
             })
         })
     }
-
+    
     render() {
         return (
             <div className="app">
---
+++ reactchat/src/README.md
@@ -0,0 +1,16 @@
+# Connecting to Chatkit 
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
