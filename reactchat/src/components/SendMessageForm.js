3f315bb3590af9082c747bb088baa19524efd6d7
sangumee <super2451894@gmail.com>
2018-07-27 15:31:02 +0900

    SendMessageForm component
    SendMessageForm component

--- reactchat/src/components/SendMessageForm.js
+++ reactchat/src/components/SendMessageForm.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
 class SendMessageForm extends React.Component {
+
+    constructor(){
+        super()
+        this.state={
+            message:''
+        }
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            message:e.target.value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        console.log(this.state.message)
+    }
     render() {
         return (
-            <form className="send-message-form">
+            <form onSubmit={this.handleSubmit}
+            className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type Your Message and hit ENTER"
                     type="text" />
             </form>
         )
