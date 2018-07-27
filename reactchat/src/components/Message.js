f800683a774bf64a287ff370e19b8bcdef20dbf0
sangumee <super2451894@gmail.com>
2018-07-27 13:56:49 +0900

    replace to Function
    replace to Function

--- reactchat/src/components/Message.js
+++ reactchat/src/components/Message.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-class Message extends React.Component {  
-    render() {
-        return (
-            <div className="message">
+function Message(props){
+    return <div className="message">
+                <div className="message-username">{props.username}</div>
+                <div className="message-text">{props.text}</div>
             </div>
-        )
-    }
 }
 
 export default Message
--- reactchat/src/components/MessageList.js
+++ reactchat/src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Message from './Message'
 
 
 class MessageList extends React.Component {
@@ -7,10 +8,7 @@ class MessageList extends React.Component {
             <div className="message-list">
                 {this.props.messages.map((message, index) => {
                     return (
-                        <div key={index} className="message">
-                            <div className="message-username">{message.senderId}</div>
-                            <div className="message-text">{message.text}</div>
-                        </div>
+                        <Message key={index} username={message.senderId} text={message.text} />
                     )
                 })}
             </div>
